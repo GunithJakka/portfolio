cdf10fbf5a61b552be1c6afcbdd5f078a8688e00
GSP23SCM93J <129034256+GSP23SCM93J@users.noreply.github.com>
2024-02-25 19:37:22 -0600

    first Commit

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const Social = () => {
+  return (
+    <div className="home__social">
+        <a href="https://www.instagram.com/jakkagunith/" className="home__social-icon" target="_blank">
+            <i className="uil uil-instagram"></i>
+        </a>
+        <a href="https://www.linkedin.com/in/jakka-gunith/" className="home__social-icon" target="_blank">
+            <i className="uil uil-linkedin"></i>
+        </a>
+        <a href="https://github.com/GunithJakka" className="home__social-icon" target="_blank">
+            <i className="uil uil-github-alt">
+             
+            </i>
+        </a>
+        <a href="" className="home__social-icon" target="_blank"></a>
+    </div>
+  )
+}
